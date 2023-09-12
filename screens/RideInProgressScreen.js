60e1e1ff120de49f77004782be8951e86f86722b
Arthur Kabera <arthurkabera@gmail.com>
2023-09-12 03:28:16 +0300

    Added, create completed transactions in driverFinances

--- screens/RideInProgressScreen.js
+++ screens/RideInProgressScreen.js
@@ -22,10 +22,11 @@ const RideInProgress = () => {
   const person = useSelector(selectPerson);
   const [isRatingModalVisible, setRatingModalVisible] = useState(false);
   const [rating, setRating] = useState(0);
+  const [driverBalance, setDriverBalance] = useState(0);
 
   console.log("The Driver", person);
 
-  // Use the useSelector hook to select the data from the currentRideSlice
+  // Use useSelector hook to select the data from the currentRideSlice
   const currentRideData = useSelector((state) => state.currentRide);
 
   // Drop Off Code
@@ -46,6 +47,187 @@ const RideInProgress = () => {
         // navigation.navigate("RideInProgressScreen");
 
         // Update Financial Data
+        // First get the Payment Type
+        // Function to check if a document with a certain ID exists, and create it if it doesn't
+        async function createOrUpdateDocument(documentId) {
+          // Check if the document already exists
+          const documentRef = db
+            .collection("driverCurrentBalance")
+            .doc(documentId);
+          const documentSnapshot = await documentRef.get();
+
+          // If Document Already Exists
+          if (documentSnapshot.exists) {
+            // Document already exists, update it
+            const existingData = documentSnapshot.data();
+            const currentBalance = existingData.balance || 0; // Default to 0 if balance doesn't exist
+
+            // Document already exists, update it
+            const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+
+            // If a Deduction was applied on the Ride
+            const rideDeduction =
+              currentRideData["currentRide"]["totalDeduction"];
+            const balAfterDeduction = 0.85 * parseInt(rideDeduction);
+
+            const updatedBalance = balAfterDeduction + currentBalance;
+
+            // Check if Payment type is not Cash and Add Amount to Balance
+            // Afterwards, Create a Payment Transactions Document
+            const paymentMethodId = parseInt(
+              currentRideData["currentRide"]["paymentMethod"]["id"]
+            );
+
+            if (paymentMethodId === 1) {
+              // It is a Cash Payment
+              // Deduction + (Total Commission)
+              const totalCommission =
+                -parseInt(
+                  currentRideData["currentRide"]["totalFareBeforeDeduction"]
+                ) * 0.15;
+              const deduction = parseInt(
+                currentRideData["currentRide"]["totalDeduction"]
+              );
+
+              const driverDues = totalCommission + deduction;
+
+              console.log("Total Commission: " + totalCommission);
+              console.log("Deduction: " + deduction);
+              console.log("Mile Owes the Driver: " + driverDues);
+
+              /*
+              console.log("Ride ID: " + currentRideData["currentRide"]["id"]);
+              console.log("Ride ID: " + currentRideData["currentRide"]["id"]);
+              console.log("Ride ID: " + currentRideData["currentRide"]["id"]);
+              console.log("Ride ID: " + currentRideData["currentRide"]["id"]);
+              */
+
+              // Create a Document to Record this Financial Transaction
+              const driverFinancesRef = db.collection("driverFinances").doc();
+
+              // Define the data for the document
+              const financeData = {
+                driverId: currentRideData["currentRide"]["driverId"],
+                paymentType:
+                  currentRideData["currentRide"]["paymentMethod"]["id"],
+                rideId: currentRideData["currentRide"]["id"],
+                riderId: currentRideData["currentRide"]["riderId"],
+                totalAmount:
+                  currentRideData["currentRide"]["totalFareBeforeDeduction"], // Replace with your values
+                totalClientPays:
+                  currentRideData["currentRide"]["totalClientPays"], // Replace with your values
+                totalDeduction:
+                  currentRideData["currentRide"]["totalDeduction"], // Replace with your values
+                driverRevenue: driverDues, // Replace with your values
+                paid: false, // Replace with your values
+                date: firebase.firestore.FieldValue.serverTimestamp(), // Use Firestore Timestamp
+              };
+
+              // Set the data in the Firestore document
+              driverFinancesRef
+                .set(financeData)
+                .then(() => {
+                  console.log("Driver Finances Document Written Successfully!");
+                })
+                .catch((error) => {
+                  console.error("Error writing document: ", error);
+                });
+            } else {
+              // It is a Cash Payment
+              // Deduction + (Total Commission)
+              const totalCommission =
+                -parseInt(
+                  currentRideData["currentRide"]["totalFareBeforeDeduction"]
+                ) * 0.15;
+              const deduction = parseInt(
+                currentRideData["currentRide"]["totalDeduction"]
+              );
+              const totalAfterDeduction =
+                parseInt(
+                  currentRideData["currentRide"]["totalFareBeforeDeduction"]
+                ) - deduction;
+              const driverDues =
+                totalCommission + deduction + totalAfterDeduction;
+
+              console.log("Total Commission: " + totalCommission);
+              console.log("Deduction: " + deduction);
+              console.log("Mile Owes the Driver: " + driverDues);
+              console.log("Mile Owes the Driver: " + driverDues);
+
+              /*
+              console.log("Ride ID: " + currentRideData["currentRide"]["id"]);
+              console.log("Ride ID: " + currentRideData["currentRide"]["id"]);
+              console.log("Ride ID: " + currentRideData["currentRide"]["id"]);
+              console.log("Ride ID: " + currentRideData["currentRide"]["id"]);
+              */
+
+              // Create a Document to Record this Financial Transaction
+              const driverFinancesRef = db.collection("driverFinances").doc();
+
+              // Define the data for the document
+              const financeData = {
+                driverId: currentRideData["currentRide"]["driverId"],
+                paymentType:
+                  currentRideData["currentRide"]["paymentMethod"]["id"],
+                rideId: currentRideData["currentRide"]["id"],
+                riderId: currentRideData["currentRide"]["riderId"],
+                totalAmount:
+                  currentRideData["currentRide"]["totalFareBeforeDeduction"], // Replace with your values
+                totalClientPays:
+                  currentRideData["currentRide"]["totalClientPays"], // Replace with your values
+                totalDeduction:
+                  currentRideData["currentRide"]["totalDeduction"], // Replace with your values
+                driverRevenue: driverDues, // Replace with your values
+                paid: false, // Replace with your values
+                date: firebase.firestore.FieldValue.serverTimestamp(), // Use Firestore Timestamp
+              };
+
+              // Set the data in the Firestore document
+              driverFinancesRef
+                .set(financeData)
+                .then(() => {
+                  console.log("Driver Finances Document Written Successfully!");
+                })
+                .catch((error) => {
+                  console.error("Error writing document: ", error);
+                });
+            }
+
+            // Update the document with new values for lastUpdated and balance
+            await documentRef.update({
+              lastUpdated: timestamp,
+              balance: updatedBalance, // Set balance to 100 (you can modify this value as needed)
+            });
+
+            console.log(`Document with ID ${documentId} updated.`);
+          } else {
+            // Document doesn't exist, create it
+            const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+            const initialBalance = 100;
+
+            // Data to be added to the document
+            const data = {
+              lastUpdated: timestamp,
+              balance: initialBalance,
+            };
+
+            // Create the document with the specified ID and data
+            await documentRef.set(data);
+
+            console.log(`Document with ID ${documentId} created.`);
+          }
+        }
+
+        // Usage
+        const documentIdToCheck = person["authID"];
+
+        createOrUpdateDocument(documentIdToCheck)
+          .then(() => {
+            console.log("Created Driver Curr-Bal Document.");
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
       })
       .catch((error) => {
         console.error("Error updating ride status:", error);
@@ -249,7 +431,7 @@ const RideInProgress = () => {
         </View>
       </View>
 
-      <Modal isVisible={isRatingModalVisible}>
+      <Modal visible={isRatingModalVisible}>
         <View style={tw`bg-white p-6 rounded-lg`}>
           <Text>Rate the rider</Text>
 
